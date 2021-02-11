04a292e4228c2744a5ae4a6d6c4e1b1368336d34
pofider <jan.blaha@hotmail.com>
2021-02-11 14:33:14 +0100

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using jsreport.Binary;
+using jsreport.Local;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rendering template");
+
+            var rs = new LocalReporting()
+                .UseBinary(JsReportBinary.GetBinary())
+                .KillRunningJsReportProcesses()
+                .Configure(cfg => cfg.FileSystemStore().BaseUrlAsWorkingDirectory())
+                .AsUtility()
+                .Create();
+
+            var report = rs.RenderByNameAsync("myTemplate", null).Result;
+
+            using (var fs = File.Create("test.pdf"))
+            {
+                report.Content.CopyTo(fs);
+            }
+        }
+    }
+}
---
+++ Reports/Program.cs
@@ -0,0 +1,36 @@
+﻿using jsreport.Binary;
+using jsreport.Local;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsreportDirectory = Path.Combine(System.Environment.CurrentDirectory.Substring(0, System.Environment.CurrentDirectory.LastIndexOf("bin")), "jsreport");
+            Console.WriteLine("Starting jsreport in " + jsreportDirectory);
+         
+            var rs = new LocalReporting()
+             .UseBinary(JsReportBinary.GetBinary())
+             .KillRunningJsReportProcesses()
+             .RunInDirectory(jsreportDirectory)
+             .Configure(cfg => cfg.CreateSamples()
+                .FileSystemStore()
+                .BaseUrlAsWorkingDirectory())
+             .AsWebServer()
+             .RedirectOutputToConsole() 
+             .Create();
+
+            rs.StartAsync().Wait();
+
+            Process.Start(new ProcessStartInfo("cmd", $"/c start http://localhost:5488"));
+
+            Console.ReadKey();
+
+            rs.KillAsync().Wait();
+        }
+    }
+}
